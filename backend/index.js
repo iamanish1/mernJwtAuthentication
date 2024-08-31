08b99fdaa0c16dabd48b02e92618d349082f86b0
iamanish1 <animetra14@gmail.com>
2024-08-31 10:02:45 +0530

    creating a testing routes

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv' ; 
 import { connectDb } from './connectiondb.js';
+import userRouter from './routes/user.routes.js';
 
 dotenv.config();
 
@@ -9,7 +10,14 @@ const app = express();
 
 connectDb()
 
+//Middleware
+
+app.use('/api',userRouter);
+
 app.listen(process.env.PORT||3000,()=>{
      console.log(`Server is running on port ${process.env.PORT}`);
  });
+
+ 
+
 
---
+++ backend/routes/user.routes.js
@@ -0,0 +1,15 @@
+import express, { Router } from 'express';
+import { test } from '../controllers/user.controller.js';
+
+const userRouter = express.Router();
+
+// TEST API GET
+userRouter.get('/test',test)
+
+// TEST API POST
+
+userRouter.post('/test2',(req,res)=>{
+res.json({message: 'API POST request is working'});
+});
+
+export default userRouter
