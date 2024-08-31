581056149f5ae55809203ce6ac6c80f0b2b57f7b
iamanish1 <animetra14@gmail.com>
2024-08-31 09:36:24 +0530

    done with user model

---
+++ backend/models/userModel.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+     username : {
+        type : 'string',
+        required : true,
+        unique : true,
+        lowercase : true,
+     },
+      email : {
+        type : 'string',
+        required : true,
+        unique : true,
+        lowercase : true,
+      },
+      password : {
+        type : 'string',
+        required : true,
+        unique :true
+      },
+},{timestamps: true,})
+
+const User = mongoose.model('User',userSchema);
+
+export {User}
